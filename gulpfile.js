a156917586a1a92d9d530f1d67f56ce87c46a8ff
Sebo Kim <sanari85@gmail.com>
2016-05-29 10:55:04 -0400

    trivial

--- gulpfile.js
+++ gulpfile.js
@@ -4,43 +4,50 @@ var cssmin = require('gulp-clean-css');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var ghPages = require('gulp-gh-pages');
 
-gulp.task('minify-html', function() {
-  return gulp.src('*.html')
-    .pipe(htmlmin({collapseWhitespace: true, removeComments: true}))
-    .pipe(gulp.dest('dist'))
+gulp.task('deploy', function() {
+  return gulp.src('./**/*')
+    .pipe(ghPages());
 });
 
-gulp.task('minify-css', function() {
-  return gulp.src('css/**/*.css')
-  .pipe(cssmin())
-  .pipe(rename({
-    suffix: '.min'
-  }))
-  .pipe(gulp.dest('dist/css'))
-});
-
-// gulp.task('minify-js1', function() {
-//   return gulp.src('js/index.js')
-//   .pipe(uglify())
+// gulp.task('minify-html', function() {
+//   return gulp.src('*.html')
+//     .pipe(htmlmin({collapseWhitespace: true, removeComments: true}))
+//     .pipe(gulp.dest('dist'))
+// });
+//
+// gulp.task('minify-css', function() {
+//   return gulp.src('css/**/*.css')
+//   .pipe(cssmin())
 //   .pipe(rename({
 //     suffix: '.min'
 //   }))
-//   .pipe(gulp.dest('dist/js'))
+//   .pipe(gulp.dest('dist/css'))
 // });
 //
-// gulp.task('minify-js2', function() {
-//   return gulp.src(['js/main.js', 'js/idb.js'])
-//   .pipe(concat('main-idb.min.js'))
-//   .pipe(uglify())
-//   .pipe(gulp.dest('dist/js'))
-// });
-
+// // gulp.task('minify-js1', function() {
+// //   return gulp.src('js/index.js')
+// //   .pipe(uglify())
+// //   .pipe(rename({
+// //     suffix: '.min'
+// //   }))
+// //   .pipe(gulp.dest('dist/js'))
+// // });
+// //
+// // gulp.task('minify-js2', function() {
+// //   return gulp.src(['js/main.js', 'js/idb.js'])
+// //   .pipe(concat('main-idb.min.js'))
+// //   .pipe(uglify())
+// //   .pipe(gulp.dest('dist/js'))
+// // });
+//
 gulp.task('watch', function() {
-    // gulp.watch('js/*.js', ['minify-js1', 'minify-js2']);
-    gulp.watch('css/*.css', ['minify-css']);
-    gulp.watch('*.html', ['minify-html']);
+    gulp.watch('src/js/*.js', ['deploy']);
+    gulp.watch('src/css/*.css', ['deploy']);
+    gulp.watch('src/*.html', ['deploy']);
+    gulp.watch('src/sw.js', ['deploy'])
 });
-
-// gulp.task('default',['minify-html', 'minify-css', 'minify-js1', 'minify-js2']);
-gulp.task('default',['minify-html', 'minify-css']);
+//
+// // gulp.task('default',['minify-html', 'minify-css', 'minify-js1', 'minify-js2']);
+// gulp.task('default',['minify-html', 'minify-css']);
--- src/sw.js
+++ src/sw.js
@@ -31,8 +31,8 @@ self.addEventListener('activate', function(event) {
 });
 
 this.addEventListener('fetch', function(event) {
-  // console.log(event);
-  console.log(event.request);
+  console.log(event);
+  // console.log(event.request);
 
   // var response;
   // event.respondWith(caches.match(event.request).catch(function() {
